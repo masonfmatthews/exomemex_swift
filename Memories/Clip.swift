92c04548a3bc014c5088ea92a417944c23662ba2
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-12-22 13:37:15 -0500

    Add clips table.

---
+++ Memories/Clip.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Clip {
+    let id : Int
+    let name: String
+}
---
+++ Memories/OldClipsController.swift
@@ -0,0 +1,45 @@
+//
+//  OldClipsController.swift
+//  Memories
+//
+//  Created by Mason Matthews on 12/22/15.
+//  Copyright © 2015 Mason F. Matthews. All rights reserved.
+//
+
+import UIKit
+
+class OldClipsController: UITableViewController {
+    
+    var clips : [Clip] = [Clip(id: 1, name: "First Clip")]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clips.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let clip = clips[indexPath.row]
+        cell.textLabel!.text = clip.name
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            clips.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+
+}

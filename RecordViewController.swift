a9498fe1b6cdc01cffdabcce0bc4258d5af382e0
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-12-29 13:22:23 -0500

    Record audio to documents.

--- RecordViewController.swift
+++ RecordViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AVFoundation
 
 class RecordViewController: UIViewController {
     
@@ -7,9 +8,12 @@ class RecordViewController: UIViewController {
         self.nameField.text = ""
     }
     @IBAction func saveRecording(sender: AnyObject) {
+        self.audioRecorder!.stop()
         let _ = CreateClipApi(clipFields: ["name" : nameField.text!])
         //TODO: Later, display something different if the API returns an error.
     }
+    
+    var audioRecorder : AVAudioRecorder?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,7 @@ class RecordViewController: UIViewController {
         button.backgroundColor = UIColor(red: 200, green: 0, blue: 0, alpha: 1)
         button.layer.cornerRadius = 0.5 * button.bounds.size.width
         // button.setImage(UIImage(named:"thumbsUp.png"), forState: .Normal)
-        button.addTarget(self, action: "buttonPressed", forControlEvents: .TouchUpInside)
+        button.addTarget(self, action: "record", forControlEvents: .TouchUpInside)
         
         view.addSubview(button)
         
@@ -29,8 +33,46 @@ class RecordViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    func buttonPressed() {
-        print("Round button pressed!")
+    func record() {
+        //init
+        let audioSession:AVAudioSession = AVAudioSession.sharedInstance()
+        
+        //ask for permission
+        if (audioSession.respondsToSelector("requestRecordPermission:")) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
+                if granted {
+                    print("granted")
+                    
+                    //set category and activate recorder session
+                    try! audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                    try! audioSession.setActive(true)
+                    
+                    //get documnets directory
+                    let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+                    let url = NSURL.fileURLWithPath(documentsDirectory + "/voiceRecording.wav")
+                    
+                    print(url)
+                    
+                    //create AnyObject of settings
+                    let settings: [String : AnyObject] = [
+                        AVFormatIDKey:Int(kAudioFormatLinearPCM), //Int required in Swift2
+                        AVSampleRateKey:44100.0,
+                        AVNumberOfChannelsKey:1,
+                        AVEncoderBitRateKey:12800,
+                        AVLinearPCMBitDepthKey:8,
+                        AVEncoderAudioQualityKey:AVAudioQuality.Max.rawValue
+                    ]
+                    
+                    //record
+                    try! self.audioRecorder = AVAudioRecorder(URL: url, settings: settings)
+                    self.audioRecorder!.record()
+                    
+                } else{
+                    print("not granted")
+                }
+            })
+        }
+        
     }
     
 }

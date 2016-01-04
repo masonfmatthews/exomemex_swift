4ca28a58229d14ea3a54b955865847004c876b34
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-01-04 10:19:18 -0500

    Add controller for play view.

---
+++ Memories/PlayClipController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class PlayClipController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}

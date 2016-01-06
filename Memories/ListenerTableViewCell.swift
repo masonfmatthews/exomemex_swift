5ac907aa69372af11bcadeb1a2f41bddead50124
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-01-06 11:15:55 -0500

    Attempt to create a table view inside a view controller.

---
+++ Memories/ListenerTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ListenerTableViewCell : UITableViewCell {
+    @IBOutlet weak var sendSwitch: UISwitch!
+    @IBOutlet weak var label: UILabel!
+
+    
+}
--- Memories/SaveClipController.swift
+++ Memories/SaveClipController.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class SaveClipController: UIViewController {
+class SaveClipController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
     var session = SessionController.sharedController.session
     var listeners : [User] = []
     
@@ -14,34 +15,20 @@ class SaveClipController: UIViewController {
         }
     }
     
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return clips.count
-//    }
-//    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-//        
-//        let clip = clips[indexPath.row]
-//        cell.textLabel!.text = clip.name
-//        return cell
-//    }
-//    
-//    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-//        // Return false if you do not want the specified item to be editable.
-//        return true
-//    }
-//    
-//    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-//        if editingStyle == .Delete {
-//            clips.removeAtIndex(indexPath.row)
-//            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-//        } else if editingStyle == .Insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-//        }
-//    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listeners.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ListenerTableViewCell
+        
+        let listener = listeners[indexPath.row]
+        cell.label!.text = listener.name
+        return cell
+    }
     
 }

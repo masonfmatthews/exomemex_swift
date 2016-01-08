c5ad72f743d14efb8989fd20be18d82cfd0eef95
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-01-08 12:43:06 -0500

    Clean up landing page.

--- Memories/LandingController.swift
+++ Memories/LandingController.swift
@@ -2,12 +2,30 @@ import UIKit
 
 class LandingController: UIViewController {
     
+    @IBOutlet weak var listenButton: UIButton!
+    @IBOutlet weak var familyButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Journeology"
+        
+        makeSettingsButton(self.listenButton)
+        makeSettingsButton(self.familyButton)
+        makeSettingsButton(self.logoutButton)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    private func makeSettingsButton(button: UIButton!) {
+        let brown = UIColor(red: 134/255, green: 83/255, blue: 39/255, alpha: 1)
+        button.setTitleColor(brown, forState: .Normal)
+        button.backgroundColor = UIColor.clearColor()
+        button.layer.borderWidth = 1
+        button.layer.borderColor = brown.CGColor
+        button.layer.cornerRadius = 4
     }
     
 }
